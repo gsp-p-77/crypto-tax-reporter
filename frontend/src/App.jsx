c923fd3cf4a1853f3ed8709450598e0842c7eb40
Gero Sparwald <gero.sparwald@gmail.com>
2025-10-09 20:44:23 +0200

    Initial changes to bring up

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,13 @@
 import { useEffect, useState } from "react";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
 export default function App() {
   const [transactions, setTransactions] = useState([]);
